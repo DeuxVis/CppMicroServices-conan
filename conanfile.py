2a2e0974beecda6b48976ffc59a92f045ff3f22a
DeuxVis <DeuxVis@fakemail.com>
2018-04-13 12:44:17 +0200

    Update to version 3.3.0 and some fixes for windows / MSVC build. SHOULD be usable in that environment.

--- conanfile.py
+++ conanfile.py
@@ -1,40 +1,68 @@
 from conans import ConanFile
+import os
+import re
+#from io import StringIO
 
 
 class CppMicroServicesConan(ConanFile):
     name = 'CppMicroServices'
-    version = '3.0.0'
+    version = '3.3.0'
+    license = "Apache License - Version 2.0"
+    description = "An OSGi-like C++ dynamic module system and service registry http://cppmicroservices.org"
     settings = ['os', 'compiler', 'build_type', 'arch']
     generators = ['cmake']
-    url = 'https://github.com/azriel91/CppMicroServices-conan.git'
+    url = 'https://github.com/DeuxVis/CppMicroServices-conan.git'
     options = {
-        'US_ENABLE_AUTOLOADING_SUPPORT': ['ON', 'OFF'],  # Enable bundle auto-loading support
-        'US_ENABLE_THREADING_SUPPORT': ['ON', 'OFF'],    # Enable threading support
-        'US_ENABLE_DEBUG_OUTPUT': ['ON', 'OFF'],         # Enable debug messages
-        'US_BUILD_SHARED_LIBS': ['ON', 'OFF'],           # Build shared libraries
-        'US_BUILD_TESTING': ['ON', 'OFF'],               # Build tests
-        'US_BUILD_EXAMPLES': ['ON', 'OFF'],              # Build example projects
+        'US_ENABLE_THREADING_SUPPORT': ['ON', 'OFF'],    # Enable threading support.
+        'US_BUILD_SHARED_LIBS': ['ON', 'OFF'],           # Build shared libraries.
+        'US_BUILD_TESTING': ['ON', 'OFF'],               # Build tests.
+        'US_BUILD_EXAMPLES': ['ON', 'OFF'],              # Build example projects.
+        'US_BUILD_DOC_HTML': ['ON', 'OFF'],              # Build the html documentation, as seen on docs.cppmicroservices.org.
+        'US_BUILD_DOC_MAN': ['ON', 'OFF'],               # Build the man pages.
     }
-    default_options = ('US_ENABLE_AUTOLOADING_SUPPORT=OFF',
-                       'US_ENABLE_THREADING_SUPPORT=OFF',
-                       'US_ENABLE_DEBUG_OUTPUT=OFF',
+    default_options = ('US_ENABLE_THREADING_SUPPORT=OFF',
                        'US_BUILD_SHARED_LIBS=ON',
                        'US_BUILD_TESTING=OFF',
-                       'US_BUILD_EXAMPLES=OFF')
-    cppmicroservices_bundles = ['core', 'httpservice', 'shellservice', 'webconsole']
+                       'US_BUILD_EXAMPLES=OFF',
+                       'US_BUILD_DOC_HTML=OFF',
+                       'US_BUILD_DOC_MAN=OFF')
+    cppmicroservices_bundles = ['framework', 'httpservice', 'shellservice', 'webconsole']
     build_dir = 'build'
+    linked_libraries_filename = 'cppmicroservices_linked_libraries.txt'
 
     def source(self):
         cppmicroservices_url = 'https://github.com/CppMicroServices/CppMicroServices.git'
-        beta_branch = 'beta-release-{version}'.format(version=self.version)
-        self.run("git clone {url} --branch {branch} --depth 1".format(url=cppmicroservices_url, branch=beta_branch))
+        release_tag = 'v{version}'.format(version=self.version)
+        self.run("git clone {url} --branch {tag} --depth 1".format(url=cppmicroservices_url, tag=release_tag))
 
     def build(self):
+        if not os.path.isdir(self.name):
+            self.source()
+
+        print ('### Settings ###')
+        for (setting, val) in self.settings.iteritems():
+            print( "{s} = {v}".format( s=setting, v=val ))
+
+        print ('### Options ###')
+        for (option, val) in self.options.iteritems():
+            print( "{o} = {v}".format( o=option, v=val ))
+
         option_defines = ' '.join("-D%s=%s" % (option, val) for (option, val) in self.options.iteritems())
+
+        if self.settings.arch == 'x86_64':
+            option_defines += ' -DCMAKE_GENERATOR_PLATFORM=x64'
+
+        option_defines += " -DCMAKE_BUILD_TYPE={type}".format(type=self.settings.build_type)
+
         self.run("cmake {src_dir} -B{build_dir} {defines}".format(src_dir=self.name,
                                                                   build_dir=self.build_dir,
                                                                   defines=option_defines))
-        self.run("cmake --build {build_dir}".format(build_dir=self.build_dir))
+        self.run("cmake --build {build_dir} --config {type}".format( build_dir=self.build_dir, type=self.settings.build_type))
+
+        linked_libraries_file_path = "{build_dir}/{filename}".format(build_dir=self.build_folder, filename=self.linked_libraries_filename)
+        with open(linked_libraries_file_path, 'w') as linked_libraries_file:
+            linked_libraries_file.write('\n'.join(self._get_linker_libraries()))
+            linked_libraries_file.close()
 
     def package(self):
         # Module headers
@@ -61,13 +89,26 @@ class CppMicroServicesConan(ConanFile):
         build_lib_dir = "{build_dir}/lib".format(build_dir=self.build_dir)
         self.copy('*.so*', dst='lib', src=build_lib_dir)  # In unix systems, the version number is appended
         self.copy('*.a', dst='lib', src=build_lib_dir)
-        self.copy('*.lib', dst='lib', src=build_lib_dir)
+        # msvc places '.lib' files under the build type (Debug/Release) subdirectory
+        self.copy('*.lib', dst='lib', src=build_lib_dir, keep_path=False)
 
         # usWebConsole is built in the bin/main directory, need to check why
         # In unix systems, the version number is appended to the shared library
-        self.copy('*.so*', dst='lib', src="{build_dir}/bin/main".format(build_dir=self.build_dir))
-        self.copy('*.dll', dst='lib', src="{build_dir}/bin".format(build_dir=self.build_dir))
+        build_bin_dir = "{build_dir}/bin".format(build_dir=self.build_dir)
+        if self.settings.os == "Windows":
+            build_bin_dir = "{build_dir}/bin/{type}".format( build_dir=self.build_dir, type=self.settings.build_type )
+        #print ("### Copying {type} binaries from build_bin_dir = '{var}' :".format(  type=self.settings.build_type, var=build_bin_dir) )
+        self.copy('*.so*', dst='lib', src=build_bin_dir)
+        self.copy('*.dll', dst='bin', src=build_bin_dir)
+        
+        # Executables
+        self.copy('usResourceCompiler3*', dst='bin', src=build_bin_dir)
+        self.copy('usShell3*', dst='bin', src=build_bin_dir)
 
+        self.copy('GettingStarted*', dst='bin', src=build_bin_dir)
+        self.copy('usTutorialDriver*', dst='bin', src=build_bin_dir)
+        self.copy('usWebConsoleDriver*', dst='bin', src=build_bin_dir)
+        
         # CMake info
         self.copy('CppMicroServicesConfig.cmake', dst='.', src=self.build_dir)
         self.copy('CppMicroServicesConfigVersion.cmake', dst='.', src=self.build_dir)
@@ -77,9 +118,19 @@ class CppMicroServicesConan(ConanFile):
         # We need to copy '*' because there are also template code files in the cmake directory
         self.copy('*', dst='cmake', src="{src_dir}/cmake".format(src_dir=self.name))
 
+        # Linked libraries file
+        self.copy(self.linked_libraries_filename, dst='.', src=self.build_dir)
+
     def package_info(self):
         """ Maybe we shouldn't link to every bundle that is built, and just link to the CppMicroServices one """
-        self.cpp_info.libs = [self.name, 'usHttpService', 'usShellService', 'usWebConsole']
+        # linked_libraries_file_path = "%s/%s" % (self.cpp_info.rootpath, self.linked_libraries_filename)
+        # with open(linked_libraries_file_path, 'r') as linked_libraries_file:
+        #     # Warning: This reads the whole file into memory, but we expect it to be small
+        #     self.cpp_info.libs = linked_libraries_file.read().split()
+        self.cpp_info.libs = ['CppMicroServices']
+        if self.settings.os == 'Linux':
+            self.cpp_info.libs += ['dl']
+
         self.cpp_info.includedirs += ["{bundle}/include".format(bundle=bundle)
                                       for bundle in self.cppmicroservices_bundles]
 
@@ -91,3 +142,35 @@ class CppMicroServicesConan(ConanFile):
         # Improvement: recursively include any directories underneath {bundle}/include
         self.cpp_info.includedirs += ["core/include/{core_private}".format(core_private=sub_dir)
                                       for sub_dir in ['bundle', 'service', 'util']]
+
+    def _get_linker_libraries(self):
+        # Extract dependencies for the CppMicroServices library, to be appended to self.cpp_info.libs
+        # See http://stackoverflow.com/a/31940106/1576773
+        self.run("cmake --graphviz={build_dir}/cppmicroservices.dot {build_dir}".format(build_dir=self.build_dir), False)
+
+        dependencies = []
+        # Old non windows compatible method kept commented out for reference
+        #dependencies = None
+        try:
+            fname = "{build_dir}/cppmicroservices.dot.{lib}".format(build_dir=self.build_dir, lib=self.name)
+            fin = open( fname, 'r' )
+
+            for line in fin:
+                out = re.sub( r'.*label="(.*)"\s.*', r'\1', line )
+                if out != line:
+                    dependencies.append( out.rstrip() )
+                    #print( "### Found dependency '" + out.rstrip() + "' in '" + fname + "'" )
+
+            #output_deps = StringIO()
+            #command = "sed -n 's/.*label=\"\\(.*\\)\"\\s.*/\\1/p' {build_dir}/cppmicroservices.dot.{lib}".format(build_dir=self.build_dir, lib=self.name)
+            #self.run(command, output_deps)
+            #dependencies = output_deps.getvalue().split()
+        finally:
+            fin.close()
+            #output_deps.close()
+            if self.settings.os == "Windows":
+                self.run("del /F {build_dir}\\cppmicroservices.dot*".format(build_dir=self.build_dir))
+            else:
+                self.run("rm -f {build_dir}/cppmicroservices.dot*".format(build_dir=self.build_dir))
+
+        return dependencies
